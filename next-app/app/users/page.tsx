3bd11d5e1e5ad30baf42aa7d9e235a0fe30b757c
eduardo1700 <edu.alex.mp@gmail.com>
2023-12-29 12:21:41 +0000

    fim curso

--- next-app/app/users/page.tsx
+++ next-app/app/users/page.tsx
@@ -1,8 +1,20 @@
 import React from 'react'
 
-const UsersPage = () => {
+interface User{
+    id:number;
+    name:string;
+}
+const UsersPage = async () => {
+   const res = await fetch('https://jsonplaceholder.typicode.com/users',{ cache:'no-store' });//revalidate:10 checka para info nova a cada 10 segundos, cache:'no-store' não guarda info na cache
+   const users: User[] = await res.json();
   return (
-      <div>UsersPage</div>
+      <>
+      <h1>Users</h1>
+      <p>{new Date().toLocaleTimeString()}</p>
+      <ul>
+        {users.map(user=><li key={user.id}>{user.name}</li>)}
+      </ul>
+      </>
   )
 }
 
